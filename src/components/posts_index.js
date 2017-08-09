26e6902303aecc84a33017c45b8425c813fb8b6d
Kelly Ng <kellyng87@gmail.com>
2017-08-09 15:26:03 -0700

    Add post to UI

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,13 +1,40 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions/index';
 
 class PostsIndex extends Component {
+
+  componentDidMount() {
+    // calls the action creator to obtain data using redux
+    this.props.fetchPosts();
+  }
+
+  renderPosts() {
+    return _.map(this.props.posts, post => {
+      return (
+        <li className='list-group-item' key={post.id}>
+          {post.title}
+        </li>
+      )
+    })
+  }
+
   render() {
     return (
       <div>
-        Posts Index
+        <h3>Posts</h3>
+        <ul className='list-group'>
+          {this.renderPosts()}
+        </ul>
       </div>
     )
   }
 }
 
-export default PostsIndex;
+function mapStateToProps(state) {
+  return { posts: state.posts };
+}
+
+// wire up action creator with this syntax instead of using mapDispatchToProps
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
