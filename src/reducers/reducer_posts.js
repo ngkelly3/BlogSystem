c2cbe1295744f2309d4c45f966177637723f727c
Kelly Ng <kellyng87@gmail.com>
2017-08-10 10:48:40 -0700

    Manage local state for better responsiveness after delete post

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,13 +1,11 @@
 
 import _ from 'lodash';
-import { FETCH_POSTS } from '../actions';
-import { FETCH_POST } from '../actions';
-import { DELETE_POST } from '../actions';
+import { FETCH_POSTS, FETCH_POST, DELETE_POST } from '../actions';
 
 export default function(state = {}, action) {
   switch(action.type) {
     case DELETE_POST:
-      return {}
+      return _.omit(state, action.payload);
     case FETCH_POST:
       // const post = action.payload.data;
       // const newState = { ...state };
