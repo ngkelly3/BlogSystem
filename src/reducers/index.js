47c1f1a7c062d675170b4b3d42280f1a95e60761
Kelly Ng <kellyng87@gmail.com>
2017-08-09 18:44:41 -0700

    Complete add new post form UI

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 import PostsReducer from './reducer_posts';
-import { reducers as formReducer } from 'redux-form';
+import { reducer as formReducer } from 'redux-form';
 
 const rootReducer = combineReducers({
   posts: PostsReducer,
