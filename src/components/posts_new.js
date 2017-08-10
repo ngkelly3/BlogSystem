d168e340ac5292b33fb7938d2ada98ac609d4509
Kelly Ng <kellyng87@gmail.com>
2017-08-09 18:35:26 -0700

    Add redux form

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
+
+  renderTitleField(field) {
+    return (
+      <div>
+        <input
+          type="text"
+          {...field.input}
+        />
+      </div>
+    );
+  }
+
   render() {
     return(
-      <div>
-        PostsNew!
-      </div>
+      <form>
+        <Field
+          name="title"
+          component={this.renderTitleField}
+        />
+      </form>
     )
   }
 
 }
 
-export default PostsNew;
+export default reduxForm({
+  form: 'PostsNewForm'
+})(PostsNew);
